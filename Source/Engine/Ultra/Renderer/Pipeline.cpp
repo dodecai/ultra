f79ecc6004e44b37e575b5d48484de52e4586435
larioteo <larioteo@gmail.com>
2020-09-13 02:47:36 +0200

    # Preparation for refactoring renderer due to vulkan support.

---
+++ Source/Engine/Ultra/Renderer/Pipeline.cpp
@@ -0,0 +1,9 @@
+﻿#include "Pipeline.h"
+
+namespace Ultra {
+
+Reference<Pipeline> Pipeline::Create(const PipelineProperties &properties) {
+    return Reference<Pipeline>();
+}
+
+}
---
+++ Source/Engine/Ultra/Renderer/Pipeline.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "Buffer.h"
+#include "Shader.h"
+
+namespace Ultra {
+
+struct PipelineProperties {
+    Reference<Shader> Shader;
+};
+
+class Pipeline {
+public:
+    Pipeline() = default;
+    virtual ~Pipeline() = default;
+
+    static Reference<Pipeline> Create(const PipelineProperties &properties);
+
+    virtual PipelineProperties &GetProperties() {}
+    virtual const PipelineProperties &GetProperties() const {}
+
+    virtual void Bind() {}
+    virtual void Invalidate() {}
+
+};
+
+}
