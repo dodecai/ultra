c43bc9e179e2e41f98d160cf6314a1c1911b93bb
larioteo <larioteo@gmail.com>
2023-04-02 16:26:09 +0200

    Updated submodules

--- 3rd-Party/3rd-Party.lua
+++ 3rd-Party/3rd-Party.lua
@@ -5,3 +5,12 @@ Headers["EnTT"] = "%{wks.location}/3rd-Party/EnTT/single_include"
 include "imgui/DearImGui.lua"
 include "entt/EnTT.lua"
 include "glad/Glad.lua"
+
+-- Internal
+-- Library["Vulkan"] = "%{wks.location}/3rd-Party/vulkan/lib/vulkan-1.lib"
+-- Library["VulkanUtils"] = "%{wks.location}/3rd-Party/vulkan/lib/VkLayer_utils.lib"
+
+-- External
+-- VULKAN_SDK_PATH = os.get('VK_SDK_PATH')
+-- Library["Vulkan"] = "C:/Program Files/VulkanSDK/1.2.189.2/Lib/vulkan-1.lib"
+
