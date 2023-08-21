06624fb88528629ecb12cc5e870e25ef3a750bea
larioteo <larioteo@gmail.com>
2023-08-22 00:15:53 +0200

    Added glm as subtree and removed unaccessary files part 3

--- Scripts/Update.bat
+++ Scripts/Update.bat
@@ -3,8 +3,11 @@
 	cls
 	cd /d "%~dp0"
 
-::: Update SubModules
-	git submodule update --remote --merge
+::: Update SubTree
+	git subtree pull --prefix=3rd-Party/glm https://github.com/OmniVortexStudios/glm.git master --squash
+	
+::: Update Main
+	git pull origin master
 	
 ::: BugFix: Detached Head
 	rem git submodule foreach git checkout master 
