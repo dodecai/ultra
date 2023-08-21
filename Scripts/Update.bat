ea8212fb88a25faf11b7b86d679cc2f86dee5386
larioteo <larioteo@gmail.com>
2023-08-22 00:36:07 +0200

    Added entt as subtree part 2

--- Scripts/Update.bat
+++ Scripts/Update.bat
@@ -4,7 +4,10 @@
 	cd /d "%~dp0"
 
 ::: Update SubTree
+	git subtree pull --prefix=3rd-Party/entt https://github.com/OmniVortexStudios/entt.git master --squash
 	git subtree pull --prefix=3rd-Party/glm https://github.com/OmniVortexStudios/glm.git master --squash
+	git subtree pull --prefix=3rd-Party/imgui https://github.com/OmniVortexStudios/imgui.git master --squash
+	git subtree pull --prefix=3rd-Party/stb https://github.com/OmniVortexStudios/stb.git master --squash
 	
 ::: Update Main
 	git pull origin master
