2208ee810e314a2a6438e1a50c7a86d71aa8480f
larioteo <larioteo@gmail.com>
2023-08-22 00:33:44 +0200

    Squashed '3rd-Party/entt/' content from commit 4bba54855
    git-subtree-dir: 3rd-Party/entt
    git-subtree-split: 4bba5485558fcfee244246f986fa2c080bf25e4c

---
+++ EnTT.lua
@@ -0,0 +1,34 @@
+﻿project "EnTT"
+	kind "StaticLib"
+	language "C++"
+    characterset "Unicode"
+    conformancemode "true"
+    cdialect "C17"
+	cppdialect "C++20"
+    staticruntime "on"
+    toolset "msc"
+    warnings "Default"
+
+	files {
+        "EnTT.lua",
+		"single_include/entt/entt.hpp",
+	}
+
+    filter { "configurations:Debug" }
+        defines { "_DEBUG" }
+		runtime "Debug"
+        symbols "On"
+
+    filter { "configurations:Release" }
+        defines { "NDEBUG" }
+		runtime "Release"
+        optimize "On"
+        symbols "Off"
+
+    filter { "configurations:Distribution" }
+        defines { "NDEBUG" }
+        optimize "On"
+        symbols "Off"
+
+	filter "system:windows"
+		systemversion "latest"
