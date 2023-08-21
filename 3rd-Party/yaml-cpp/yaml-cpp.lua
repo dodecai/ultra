bb0516840b964a6b6178c6c61d63d222133c55f4
larioteo <larioteo@gmail.com>
2023-08-22 01:48:47 +0200

    Fixed yaml-cpp build

--- 3rd-Party/yaml-cpp/yaml-cpp.lua
+++ 3rd-Party/yaml-cpp/yaml-cpp.lua
@@ -1,4 +1,4 @@
-project "yaml-cpp"
+﻿project "yaml-cpp"
 	kind "StaticLib"
 	language "C++"
     characterset "MBCS"
@@ -9,9 +9,20 @@ project "yaml-cpp"
     toolset "msc"
     warnings "Default"
 
+    defines {
+        "YAML_CPP_STATIC_DEFINE",
+        "YAML_CPP_NO_CONTRIB",
+    }
+
 	files {
+        "**.h",
+        "**.cpp",
         "yaml-cpp.lua",
 	}
+    
+    includedirs {
+        "include",
+    }
 
     filter { "configurations:Debug" }
         defines { "_DEBUG" }
